1f89c7ebc518c375430011ff55dd2f619d09f0dd
Charlie <cst1g16@soton.ac.uk>
2018-03-25 15:23:41 +0100

    Remove unused import statements in Pi code and Comment the uncommented sections

--- Piware.py
+++ Piware.py
@@ -2,18 +2,22 @@
 
 import cv2
 import socket
-import threading
-import numpy as np
-from time import sleep
 
+# Creates and binds to the socket used for communicating with the computer
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 9001))
+
+# Sets the maximum number of connections to 5
 s.listen(5)
+
+# Waits and accepts the first one to attempt communication
 conn, addr = s.accept()
 
 # Open Webcam
-cap = cv2.VideoCapture(0)  # 1 signifies the second available camera device
-fourcc = cv2.cv.FOURCC(*"XVID")  # The protocol used for video
+cap = cv2.VideoCapture(0)  # 0 signifies the first available camera device
+
+# The protocol used for video
+fourcc = cv2.cv.FOURCC(*"XVID")
 
 camon = True
 while camon:
@@ -26,5 +30,6 @@ while camon:
         cv2.imshow("frame", frame)
         cv2.waitKey(1)
 
+# Destroy the things which are no longer being used
 cap.release()
 cv2.destroyAllWindows()
